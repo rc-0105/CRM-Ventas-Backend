c58f79c0afd54b7e6ef2d51338580d6e637f2eb6
rc-0105 <rcarrero@unbosque.edu.co>
2026-01-12 19:27:37 -0500

    Initial commit

---
+++ src/main/java/com/rcb004/crm/crm_ventas_backend/CrmVentasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rcb004.crm.crm_ventas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmVentasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmVentasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcb004/crm/crm_ventas_backend/CrmVentasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcb004.crm.crm_ventas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmVentasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
