3a541570e1140a2747f5854580ebf4e175572ee0
rc-0105 <rcarrero@unbosque.edu.co>
2026-01-15 19:28:25 -0500

    Spring Security sin base de datos

---
+++ src/main/java/com/rcb004/crm/crm_ventas_backend/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.rcb004.crm.crm_ventas_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpsecurity) throws Exception {
+
+
+
+        return httpsecurity
+        .csrf(crsf -> crsf.disable())
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(http -> {
+            http.requestMatchers(HttpMethod.GET, "/auth/hello").permitAll();
+            http.requestMatchers(HttpMethod.GET, "/auth/hello-secured").hasAuthority("READ");
+
+            http.anyRequest().denyAll();
+            }
+        )
+        
+        
+        .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(null);
+        provider.setUserDetailsService(null);
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    
+
+}
---
+++ src/main/java/controller/TestAuthController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/auth")
+public class TestAuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Auth Controller!";
+    }
+    
+
+    @GetMapping("/hello-secured")
+    public String helloSecured() {
+        return "Hello from Secured Auth Controller!";
+    }
+
+}
